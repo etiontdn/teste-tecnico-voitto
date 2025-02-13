2848c9f3c74e891d27fc187a3fe920db968a9875
João J <etiontdn@gmail.com>
2025-02-13 15:15:33 -0300

    teste inicial e formatação

--- matricula.js
+++ matricula.js
@@ -1,19 +1,19 @@
 function getTotalAlunosPorCurso(matriculas) {
-  // Usando reduce para criar o objeto { "curso": totalAlunos}
-  const totalMatriculas = matriculas.reduce((total, matricula) => {
-    const { curso } = matricula;
+    // Usando reduce para criar o objeto { "curso": totalAlunos}
+    const totalMatriculas = matriculas.reduce((total, matricula) => {
+        const { curso } = matricula;
 
-    // Se ainda não achamos nenhuma matrícula desse curso antes começamos com 0
-    if (total[curso] === undefined) {
-      total[curso] = 0;
-    }
+        // Se ainda não achamos nenhuma matrícula desse curso antes começamos com 0
+        if (total[curso] === undefined) {
+            total[curso] = 0;
+        }
 
-    // Adiciona +1 ao total do curso atual
-    total[curso]++;
-    return total;
-  }, {});
-  return totalMatriculas;
+        // Adiciona +1 ao total do curso atual
+        total[curso]++;
+        return total;
+    }, {});
+    return totalMatriculas;
 }
 module.exports = {
-    getTotalAlunosPorCurso
-}
+    getTotalAlunosPorCurso,
+};
