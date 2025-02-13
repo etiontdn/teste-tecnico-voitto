33e36bb927c2ebfa11d9dc6b9792851e19aa8bca
João J <etiontdn@gmail.com>
2025-02-13 15:28:16 -0300

    formatação

--- testes/matricula.test.js
+++ testes/matricula.test.js
@@ -14,8 +14,10 @@ test("inicial", () => {
         "React do Zero": 2,
         "Node.js para Backend": 1,
     });
+});
 
-    matriculas = [
+test("novos", () => {
+    let matriculas = [
         { id: 1, curso: "Python para Iniciantes", aluno: "Alice" },
         { id: 2, curso: "Python para Iniciantes", aluno: "Bob" },
         { id: 3, curso: "Banco de Dados SQL", aluno: "Carlos" },
@@ -27,5 +29,36 @@ test("inicial", () => {
         "Python para Iniciantes": 2,
         "Banco de Dados SQL": 2,
         "JavaScript Avançado": 1,
+    });
+
+    matriculas = [
+        { id: 1, curso: "HTML e CSS", aluno: "Fernanda" },
+        { id: 2, curso: "HTML e CSS", aluno: "Gabriel" },
+        { id: 3, curso: "Node.js para Backend", aluno: "Hugo" },
+        { id: 4, curso: "React Avançado", aluno: "Isabela" },
+        { id: 5, curso: "React Avançado", aluno: "João" },
+        { id: 6, curso: "HTML e CSS", aluno: "Karina" },
+    ];
+
+    expect(getTotalAlunosPorCurso(matriculas)).toEqual({
+        "HTML e CSS": 3,
+        "Node.js para Backend": 1,
+        "React Avançado": 2,
+    });
+
+    matriculas = [
+        { id: 1, curso: "Vue.js Essencial", aluno: "Lucas" },
+        { id: 2, curso: "Vue.js Essencial", aluno: "Mariana" },
+        { id: 3, curso: "Vue.js Essencial", aluno: "Nicolas" },
+        { id: 4, curso: "Django com Python", aluno: "Olívia" },
+        { id: 5, curso: "Django com Python", aluno: "Pedro" },
+        { id: 6, curso: "Django com Python", aluno: "Quésia" },
+        { id: 7, curso: "Banco de Dados NoSQL", aluno: "Rafael" },
+    ];
+
+    expect(getTotalAlunosPorCurso(matriculas)).toEqual({
+        "Vue.js Essencial": 3,
+        "Django com Python": 3,
+        "Banco de Dados NoSQL": 1,
     });
 });
