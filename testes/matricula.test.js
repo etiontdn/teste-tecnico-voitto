16aa2ef3e30bc09b1ce8f0934f5572c52e3f9463
João J <etiontdn@gmail.com>
2025-02-13 15:26:21 -0300

    novos testes

--- testes/matricula.test.js
+++ testes/matricula.test.js
@@ -1,7 +1,7 @@
 const { getTotalAlunosPorCurso } = require("../matricula.js");
 
 test("inicial", () => {
-    const matriculas = [
+    let matriculas = [
         { id: 1, curso: "JavaScript Avançado", aluno: "Alice" },
         { id: 2, curso: "React do Zero", aluno: "Bob" },
         { id: 3, curso: "JavaScript Avançado", aluno: "Carlos" },
@@ -13,5 +13,19 @@ test("inicial", () => {
         "JavaScript Avançado": 2,
         "React do Zero": 2,
         "Node.js para Backend": 1,
+    });
+
+    matriculas = [
+        { id: 1, curso: "Python para Iniciantes", aluno: "Alice" },
+        { id: 2, curso: "Python para Iniciantes", aluno: "Bob" },
+        { id: 3, curso: "Banco de Dados SQL", aluno: "Carlos" },
+        { id: 4, curso: "Banco de Dados SQL", aluno: "Daniela" },
+        { id: 5, curso: "JavaScript Avançado", aluno: "Eduardo" },
+    ];
+
+    expect(getTotalAlunosPorCurso(matriculas)).toEqual({
+        "Python para Iniciantes": 2,
+        "Banco de Dados SQL": 2,
+        "JavaScript Avançado": 1,
     });
 });
