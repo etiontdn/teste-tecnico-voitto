afb0eb32cd532c984b01b119104887ba1d829409
João J <etiontdn@gmail.com>
2025-02-13 15:29:18 -0300

    testes com tamanho maiores

--- testes/matricula.test.js
+++ testes/matricula.test.js
@@ -62,3 +62,39 @@ test("novos", () => {
         "Banco de Dados NoSQL": 1,
     });
 });
+
+test("gigante", () => {
+    let matriculas = [
+        { id: 1, curso: "JavaScript Avançado", aluno: "Alice" },
+        { id: 2, curso: "React do Zero", aluno: "Bob" },
+        { id: 3, curso: "JavaScript Avançado", aluno: "Carlos" },
+        { id: 4, curso: "Node.js para Backend", aluno: "Daniela" },
+        { id: 5, curso: "React do Zero", aluno: "Eduardo" },
+        { id: 6, curso: "HTML e CSS", aluno: "Fernanda" },
+        { id: 7, curso: "HTML e CSS", aluno: "Gabriel" },
+        { id: 8, curso: "Node.js para Backend", aluno: "Hugo" },
+        { id: 9, curso: "React Avançado", aluno: "Isabela" },
+        { id: 10, curso: "React Avançado", aluno: "João" },
+        { id: 11, curso: "HTML e CSS", aluno: "Karina" },
+        { id: 12, curso: "Vue.js Essencial", aluno: "Lucas" },
+        { id: 13, curso: "Vue.js Essencial", aluno: "Mariana" },
+        { id: 14, curso: "Vue.js Essencial", aluno: "Nicolas" },
+        { id: 15, curso: "Django com Python", aluno: "Olívia" },
+        { id: 16, curso: "Django com Python", aluno: "Pedro" },
+        { id: 17, curso: "Django com Python", aluno: "Quésia" },
+        { id: 18, curso: "Banco de Dados NoSQL", aluno: "Rafael" },
+        { id: 19, curso: "Banco de Dados NoSQL", aluno: "Sofia" },
+        { id: 20, curso: "Banco de Dados NoSQL", aluno: "Tiago" },
+    ];
+
+    expect(getTotalAlunosPorCurso(matriculas)).toEqual({
+        "JavaScript Avançado": 2,
+        "React do Zero": 2,
+        "Node.js para Backend": 2,
+        "HTML e CSS": 3,
+        "React Avançado": 2,
+        "Vue.js Essencial": 3,
+        "Django com Python": 3,
+        "Banco de Dados NoSQL": 3,
+    });
+});
